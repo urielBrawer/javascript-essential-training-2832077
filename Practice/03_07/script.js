e4b7a14e1578c994cecb81609290b5a98c362143
Brawer Uriel (MP-F-CHS-FQ-BA) <uriel.brawer@sbb.ch>
2025-02-10 12:49:28 +0100

    first practice done

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,30 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+
+const house = {
+    numberOfRooms: 3,
+    Roofless: false,
+    Room: {
+        numberOfWindows:2,
+        bedroom: false,
+    },
+    blowOfRoof: function () {
+        this.Roofless = true;
+        console.log("The roof goes booom!")
+    }
+}
+
+const markup = (backpack) => {
+    return `
+    <div>
+      Roof blown of: ${house.Roofless}
+
+    </div>
+  `;
+  };
+
+const main = document.createElement("main");
+main.innerHTML = markup(house);
+document.body.appendChild(main);
