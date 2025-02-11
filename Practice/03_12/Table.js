edba77ce87edddf9baef7cbe9db172e029d3c505
Brawer Uriel (MP-F-CHS-FQ-BA) <uriel.brawer@sbb.ch>
2025-02-11 12:56:02 +0100

    TODO: why cant I see OfficeTable in the console. I want to see the different instances of my table class

---
+++ Practice/03_12/Table.js
@@ -0,0 +1,11 @@
+class Table {
+    constructor(height,width) {
+        this.height = height;
+        this.width = width;
+    }
+    setHeight (height) {
+        this.height = height;
+    }
+}
+
+export default Table;
--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,12 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+import Table from "./Table.js";
+
+const OfficeTable = new Table(10,10);
+const OfficeTable2 = new Table(10,10);
+console.log(OfficeTable);
+console.log(OfficeTable.height);
+OfficeTable.setHeight(5);
+console.log(OfficeTable2.height);
